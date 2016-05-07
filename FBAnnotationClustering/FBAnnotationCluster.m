cd8f0a0b4dcea3f1d3111ec101b7fee140bf3a5d
Matt Mo <matthew.l.mo@gmail.com>
2016-05-07 12:52:30 -0700

    [Equality] - clusters constantly get reloaded, even if they are the same

--- FBAnnotationClustering/FBAnnotationCluster.m
+++ FBAnnotationClustering/FBAnnotationCluster.m
@@ -10,4 +10,36 @@
 
 @implementation FBAnnotationCluster
 
+- (BOOL) isEqual:(id)object {
+    if (self == object) {
+        return YES;
+    } else if (![object isKindOfClass:[self class]]) {
+        return NO;
+    } else {
+        return [self isEqualAnnotation:object];
+    }
+}
+
+- (BOOL) isEqualAnnotation:(FBAnnotationCluster *)annotation {
+    if (self.coordinate.latitude != annotation.coordinate.latitude) {
+        return NO;
+    }
+    if (self.coordinate.longitude != annotation.coordinate.longitude) {
+        return NO;
+    }
+    
+    NSSet *set1 = [NSSet setWithArray:self.annotations];
+    NSSet *set2 = [NSSet setWithArray:annotation.annotations];
+    BOOL sameAnnotations = [set1 isEqualToSet:set2];
+    
+    return sameAnnotations;
+}
+
+#define NSUINT_BIT (CHAR_BIT * sizeof(NSUInteger))
+#define NSUINTROTATE(val, howmuch) ((((NSUInteger)val) << howmuch) | (((NSUInteger)val) >> (NSUINT_BIT - howmuch)))
+
+- (NSUInteger) hash {
+    return NSUINTROTATE(_coordinate.latitude, NSUINT_BIT / 3) ^ NSUINTROTATE(_coordinate.longitude, NSUINT_BIT * 2/ 3) ^ [_annotations hash];
+}
+
 @end
